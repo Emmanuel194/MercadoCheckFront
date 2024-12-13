8635b2c24f57cbc390905f75cf85b29a776357a2
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2024-12-12 23:03:10 -0300

    Second commit

--- src/components/login.js
+++ src/components/login.js
@@ -1,29 +1,68 @@
-import React, { useState } from 'react';
-import './Login.css'; 
+import React, { useState } from "react";
+import { FaGoogle, FaGithub, FaLinkedin } from "react-icons/fa";
+import "./Login.css";
 
-function Login({ onLogin }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function Login() {
+  // Removido o `onLogin` da função
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   
-    onLogin();
+    // Apenas evitar a submissão do formulário por enquanto
   };
 
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit}>
         <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label>E-mail:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu email"
+          />
         </div>
         <div>
           <label>Senha:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua senha"
+          />
         </div>
         <button type="submit">Entrar</button>
-        <button type="button" className="signup-button">Cadastre-se</button>
+        <button type="button" className="signup-button">
+          Cadastre-se
+        </button>
+        <div className="social-login">
+          <button
+            className="social-button google-button"
+            onClick={() =>
+              (window.location.href = "http://localhost:3000/auth/google")
+            }
+          >
+            <FaGoogle />
+          </button>
+          <button
+            className="social-button github-button"
+            onClick={() =>
+              (window.location.href = "http://localhost:3000/auth/github")
+            }
+          >
+            <FaGithub />
+          </button>
+          <button
+            className="social-button linkedin-button"
+            onClick={() =>
+              (window.location.href = "http://localhost:3000/auth/linkedin")
+            }
+          >
+            <FaLinkedin />
+          </button>
+        </div>
       </form>
     </div>
   );
